9be40122d0a8c62f6719df3500c6aed9472efdf2
Sérgio Guilherme <187855489+umdevaprendiz@users.noreply.github.com>
2026-04-29 21:17:35 -0300

    Criando dto

--- src/main/java/com/example/financialbank/controller/AccountController.java
+++ src/main/java/com/example/financialbank/controller/AccountController.java
@@ -1,4 +1,30 @@
-package com.example.financialbank;
+package com.example.financialbank.controller;
 
+import com.example.financialbank.model.Account;
+import com.example.financialbank.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bank")
 public class AccountController {
+    private final AccountService service;
+
+    public AccountController(AccountService service){
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> createAccount(@RequestBody Account account){
+       service.createAccount();
+        return ResponseEntity.ok("Sua conta foi criada!");
+
+    }
+
+
+
 }
